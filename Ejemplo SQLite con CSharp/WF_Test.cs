2675acb9dd2f70a20475cb76c9cdb38186fbb9ea
charlen maximo calero huaman <maxmaster_57@hotmail.com>
2018-09-21 17:23:42 -0500

    V2109.1723

--- Ejemplo SQLite con CSharp/WF_Test.cs
+++ Ejemplo SQLite con CSharp/WF_Test.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Data.SQLite;
+using System.IO;
 
 namespace Ejemplo_SQLite_con_CSharp
 {
@@ -22,20 +23,49 @@ namespace Ejemplo_SQLite_con_CSharp
 
         private void button1_Click(object sender, EventArgs e)
         {
-            SQLiteConnection
-            m_dbConnection =new SQLiteConnection("Data Source=BDSQLite/BDFacturador.sqlite;Version=3;");
-            m_dbConnection.Open();
+            // colocar la ruta fisica xe la base de datos en sqlite.
 
-         
-            string sql = "select * from TXXXX_BANDFACT";
-            SQLiteCommand command = new SQLiteCommand(sql, m_dbConnection);
-            SQLiteDataReader reader = command.ExecuteReader();
-            while (reader.Read())
-                Console.WriteLine("Name: " + reader["name"] + "\tScore: " + reader["score"]);
+            string pathToFolder = "C:\\BDFacturador.db";
+
+            SQLiteConnection
+            m_dbConnection =new SQLiteConnection("Data Source="+ pathToFolder + ";Version=3;");
+            m_dbConnection.Open();
+                     
+            //string sql = "select num_ruc,tip_docu,substr(FEC_CARG,7)||'-'||substr(FEC_CARG,4,2)||'-'||substr(FEC_CARG,1,2) AS FECHA  from TXXXX_BANDFACT";
+
+            string sql = "select *  from TXXXX_PARAM";
+            SQLiteCommand cmd = new SQLiteCommand(sql, m_dbConnection);
+            // SQLiteDataReader reader = cmd.ExecuteReader();
+
+
+            DataTable table = new DataTable("BdFacturador");
+
+        
+            using (SQLiteDataAdapter dr = new SQLiteDataAdapter(cmd))
+            {
+                using (new SQLiteCommandBuilder(dr))
+                {
+                    dr.Fill(table);
+                    dr.Update(table);
+                }
+
+            }
+
+            dataGridView1.DataSource= table;
 
         }
 
         private void WF_Test_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
         {
 
         }
